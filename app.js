531049a219744829cf23fd5f813393d6f44f8038
Shania <shaniaantezana@gmail.com>
2022-10-24 08:16:23 -0400

    lab done

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ app.use(express.static('public'))
 app.get('/',(resquest,response,next)=>{
   response.sendFile(path.resolve("./views/home-page.html"))
 })
+app.get('/about',(request,response,next)=>{
+    response.sendFile(path.resolve("./views/about-page.html"))
+})
+app.get('/works',(request,response,next)=>{
+    response.sendFile(path.resolve("./views/works-page.html"))
+})
+app.get('/photos',(request,response,next)=>{
+  response.sendFile(path.resolve("./views/gallery-page.html"))
+})
 app.listen(3000,()=>{ console.log('Server Started')})
